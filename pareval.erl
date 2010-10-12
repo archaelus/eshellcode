0a190788b74b681c13749664b6cc626118d654c7
Geoff Cant <gcant@ngmoco.com>
2010-10-12 16:24:46 -0700

    Adds a Pareval that works with async calls.

--- pareval.erl
+++ pareval.erl
@@ -9,3 +9,10 @@ Pareval = fun (N, F) ->
                   rpc:pmap({erlang, apply}, [[]],
                            [ F || _ <- lists:seq(1, N) ])
           end.
+
+f(Pareval2).
+Pareval2 = fun (N, F) ->
+                   Promises = [ rpc:async_call(node(), erlang, apply, [F, []])
+                                || _ <- lists:seq(1, N) ],
+                   [ rpc:yield(P) || P <- Promises ]
+           end.
