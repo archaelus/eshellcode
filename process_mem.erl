f14cd08b78ee3433f3f399ad834200c2773e554c
Geoff Cant <gcant@ngmoco.com>
2011-01-24 15:04:52 -0800

    Adding a process memory analysis script..

---
+++ process_mem.erl
@@ -0,0 +1,21 @@
+%% @copyright Geoff Cant 2010, 2011
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc Process memory analysis.
+%% @end
+
+f(Info).
+Info = fun (P,I) ->
+               element(2, process_info(P,I))
+       end.
+
+f(TopN).
+TopN = fun (N) ->
+               element(1, lists:split(N, lists:reverse(lists:keysort(2, [{P, Info(P, memory)} || P <- erlang:processes(), is_process_alive(P)]))))
+       end.
+
+f(MoreInfo).
+MoreInfo = fun (N) ->
+                   [{P, (Mem / (1024*1024)), Info(P, message_queue_len), process_info(P, registered_name)}
+                    || {P, Mem} <- TopN(N)]
+           end.
