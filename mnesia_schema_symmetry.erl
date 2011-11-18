009725ca30512516875efc6d76049ca583a63253
Geoff Cant <nem@erlang.geek.nz>
2011-11-18 15:51:35 -0800

    Adding mnesia cluster schema diffing shellcode.

--- mnesia_schema_symmetry.erl
+++ mnesia_schema_symmetry.erl
@@ -4,6 +4,22 @@
 %% @doc Alter table copy types to match types on a specific node.
 %% @end
 
+f(SchemaDiff).
+SchemaDiff = fun (IdealNode, OtherNodes) ->
+                     Tables = rpc:call(IdealNode, mnesia, system_info, [tables]),
+                     TableTypes = [{T, rpc:call(IdealNode, mnesia, table_info, [T, storage_type])}
+                                   || T <- Tables],
+                     [{Table, Type, [ case rpc:call(N, mnesia, table_info, [Table, storage_type]) of
+                                          Type -> {ok, N};
+                                          unknown ->
+                                              {missing, N};
+                                          Other ->
+                                              {different_type, Other, N}
+                                      end
+                                      || N <- OtherNodes]}
+                      || {Table, Type} <- TableTypes]
+             end.
+
 f(Rebalance).
 Rebalance = fun (IdealNode, OtherNodes) ->
                     Tables = rpc:call(IdealNode, mnesia, system_info, [tables]),
