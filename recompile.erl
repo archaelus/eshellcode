10de68fd997e3281e4e81274529cb44cb1c16963
Geoff Cant <nem@erlang.geek.nz>
2012-01-16 18:35:14 -0800

    Module recompilation snippet.

---
+++ recompile.erl
@@ -0,0 +1,16 @@
+%% @copyright Geoff Cant 2011
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc Convenience recompilation for modules not in cwd.
+%% @end
+
+f(Recompile).
+Recompile = fun(Mod, ExtraOpts) ->
+                    Outdir = filename:dirname(code:which(Mod)),
+                    Compile = Mod:module_info(compile),
+                    OldTime = proplists:get_value(time, Compile),
+                    Opts = proplists:get_value(options, Compile),
+                    case c:c(proplists:get_value(source, Compile),
+                             ExtraOpts ++ [{outdir, OutDir}] ++ Opts) of
+                        
+            end.
